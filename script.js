d382a9abedf1e2a2b5d6b183830fe0a2ce9a555c
Ricedog4323 <luanaaraujopinto1994@gmail.com>
2025-01-11 15:30:19 -0300

    1.2

--- script.js
+++
@@ -1,6 +0,0 @@
-const close = document.getElementById("popup-close")
-const popup = document.getElementById("popup")
-
-close.addEventListener('click', function() {
-    popup.style.display = 'none';
-  });
